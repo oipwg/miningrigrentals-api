f5092110bc0c7356c6f38011324a8f3d92ba1cbb
OstlerDev <ostlerdev@me.com>
2016-06-29 20:37:08 -0600

    Push up skeleton API code

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # miningrigrentals-api
-Node JS Module interface with the Mining Rig Rentals API easily
+`miningrigrentals-api` is a simple, easy to use way to interface with the API provided by Mining Rig Rentals.
+
+# Sample Usage Code
+```javascript
+var MiningRigRentalsAPI = require('miningrigrentals-api');
+
+var MRRAPI = new MiningRigRentalsAPI('api-key', 'api-secret');
+
+// Get all rigs
+MRRAPI.listRigs(function(rigList){
+	console.log(rigList);
+});
+
+// List only scrypt rigs
+MRRAPI.listRigs({ type: 'scrypt' }, function(rigList){
+	console.log(rigList);
+});
+```
+
+# More Info
+You can view the API docs for Mining Rig Rentals here: https://www.miningrigrentals.com/apidoc
---
+++ index.js
@@ -0,0 +1,46 @@
+var request = require('request');
+
+function MRRAPI(key, secret){
+	this.key = key;
+	this.secret = secret;
+}
+
+MRRAPI.prototype.listRigs = function(args, callback){
+
+}
+
+MRRAPI.prototype.getRigDetail = function(id, callback){
+	
+}
+
+MRRAPI.prototype.listMyRigs = function(callback){
+	
+}
+
+MRRAPI.prototype.listMyRentals = function(callback){
+	
+}
+
+MRRAPI.prototype.getRentalDetail = function(id, callback){
+	
+}
+
+MRRAPI.prototype.updateRig = function(args, callback){
+	
+}
+
+MRRAPI.prototype.rentRig = function(id, length, profileid, callback){
+	
+}
+
+MRRAPI.prototype.getBalance = function(callback){
+	
+}
+
+MRRAPI.prototype.listFavoritePools = function(callback){
+	
+}
+
+MRRAPI.prototype.listProfiles = function(callback){
+	
+}
